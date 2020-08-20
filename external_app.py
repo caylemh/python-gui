b6f9d226dd419e0d97c0c5bcee5a4c08f637a922
Caylem <caylemh@gmail.com>
2020-08-20 17:09:55 +0200

    Opening an External Application with Python

---
+++ external_app.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter import filedialog
+import os
+
+root = Tk()
+root.title('External Program')
+root.geometry('400x600')
+
+def open_program():
+    my_program = filedialog.askopenfilename()
+    my_label.config(text=my_program)
+    os.system('"%s"' % my_program)
+
+my_button = Button(root, text='Open Program', command=open_program)
+my_button.pack(pady=20)
+
+my_label = Label(root, text='')
+my_label.pack(pady=20)
+
+root.mainloop()

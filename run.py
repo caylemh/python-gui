dd19fb28302f7dc92d2fa8dfdf2ad7157104790f
caylemh <caylemh@gmail.com>
2020-08-24 15:25:02 +0200

    Adding Labels & Buttons

--- run.py
+++ run.py
@@ -1,7 +1,35 @@
-from tkinter import *
-from tkinter.ttk import *
 
-root = Tk()
-label = Label(root, text="Hello World")
-label.pack()
-root.mainloop()
+#======================
+# imports
+#======================
+import tkinter as tk
+from tkinter import ttk
+
+# Create instance
+window = tk.Tk()   
+
+# Add a title       
+window.title("Python GUI")
+
+# Add a Label
+
+a_label = ttk.Label(window, text="A Label")
+a_label.grid(column=0, row=0)
+
+# Button click event
+
+def click_me():
+    action.configure(text="I have been clicked")
+    a_label.configure(text='A Red Label')
+    a_label.configure(foreground='red')
+
+# Adding a button
+
+action = ttk.Button(window, text="Click Here", command=click_me)
+action.grid(column=0, row=1)
+
+
+#======================
+# Start GUI
+#======================
+window.mainloop()

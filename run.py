c689c68c093522650089dbd945d5979b93aeb429
Caylem <caylemh@gmail.com>
2020-08-24 22:05:33 +0200

    Refactoring

--- run.py
+++ run.py
@@ -61,29 +61,36 @@ check3.select()
 check3.grid(column=2, row=3, sticky=tk.W)
 
 # Global Variables for Radio Buttons
-COLOR1 = 'Blue'
-COLOR2 = 'Gold'
-COLOR3 = 'Red'
+colors = ['Blue', 'Gold', 'Red']
 
 # Radio Button function
 def radCall():
     radSel = radVar.get()
-    if radSel == 1:
-        window.configure(background=COLOR1)
-    elif radSel == 2:
-        window.configure(background=COLOR2)
-    elif radSel == 3:
-        window.configure(background=COLOR3)
+    if radSel == 0: window.configure(background=colors[0])
+    elif radSel == 1: window.configure(background=colors[1])
+    elif radSel == 2: window.configure(background=colors[2])
 
 # Radio Buttons
 radVar = tk.IntVar()
+radVar.set(99)
 
-rad1 = tk.Radiobutton(window, text="COLOR 1", variable=radVar, value=1, command=radCall)
-rad1.grid(column=0, row=4, sticky=tk.W, columnspan=3)
-rad2 = tk.Radiobutton(window, text="COLOR 2", variable=radVar, value=2, command=radCall)
-rad2.grid(column=1, row=4, sticky=tk.W, columnspan=3)
-rad3 = tk.Radiobutton(window, text="COLOR 3", variable=radVar, value=3, command=radCall)
-rad3.grid(column=2, row=4, sticky=tk.W, columnspan=3)
+for col in range(5):
+    curRad = tk.Radiobutton(window, text=colors[col], variable=radVar, value=col, command=radCall)
+    curRad.grid(column=col, row=4, sticky=tk.W)
+
+# rad1 = tk.Radiobutton(window, text="COLOR 1", variable=radVar, value=1, command=radCall)
+# rad1.grid(column=0, row=4, sticky=tk.W, columnspan=3)
+# rad2 = tk.Radiobutton(window, text="COLOR 2", variable=radVar, value=2, command=radCall)
+# rad2.grid(column=1, row=4, sticky=tk.W, columnspan=3)
+# rad3 = tk.Radiobutton(window, text="COLOR 3", variable=radVar, value=3, command=radCall)
+# rad3.grid(column=2, row=4, sticky=tk.W, columnspan=3)
+
+# ScrollText Widget
+scrol_w = 50
+scrol_h = 3
+
+scroll = scrolledtext.ScrolledText(window, width=scrol_w, height=scrol_h, wrap=tk.WORD)
+scroll.grid(column=0, row=5, columnspan=3)
 
 name_entered.focus()  # Placing focus into the Entry box
 

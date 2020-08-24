5ee6b23b3ce7a48b71ddf4c09b625a998d22aa27
Caylem <caylemh@gmail.com>
2020-08-24 20:45:39 +0200

    Adding ComboBoxes

--- run.py
+++ run.py
@@ -25,14 +25,22 @@ ttk.Label(text="Enter a name: ").grid(column=0, row=0)
 # Adding a Text box Entry widget
 name = tk.StringVar()
 name_entered = ttk.Entry(window, width=12, textvariable=name)
-name_entered.focus()  # Placing focus into the Entry box
 name_entered.grid(column=0, row=1)
+
+# Adding ComboBoxes
+b_label = ttk.Label(window, text="Choose a number: ").grid(column=1, row=0)
+number = tk.StringVar
+number_chosen = ttk.Combobox(window, width=12, textvariable=number)
+number_chosen['value'] = (1,2,3,42,100)
+number_chosen.grid(column=1, row=1)
+number_chosen.current(0)
 
 # Adding a button
 action = ttk.Button(window, text="Click Here", command=click_me)
-action.grid(column=1, row=1)
+action.grid(column=2, row=1)
 action.configure(state='disabled')
 
+name_entered.focus()  # Placing focus into the Entry box
 
 #======================
 # Start GUI

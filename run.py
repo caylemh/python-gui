86ebee6fc6307ce6c981c86afa9d36cb52d29278
caylemh <caylemh@gmail.com>
2020-08-24 15:47:24 +0200

    Adding an Entry box and placing Focus in it

--- run.py
+++ run.py
@@ -12,21 +12,25 @@ window = tk.Tk()
 window.title("Python GUI")
 
 # Add a Label
-
 a_label = ttk.Label(window, text="A Label")
 a_label.grid(column=0, row=0)
 
 # Button click event
-
 def click_me():
-    action.configure(text="I have been clicked")
-    a_label.configure(text='A Red Label')
-    a_label.configure(foreground='red')
+    action.configure(text='Hello ' + name.get())
+
+#Changing our label
+ttk.Label(text="Enter a name: ").grid(column=0, row=0)
+
+# Adding a Text box Entry widget
+name = tk.StringVar()
+name_entered = ttk.Entry(window, width=12, textvariable=name)
+name_entered.focus()  # Placing focus into the Entry box
+name_entered.grid(column=0, row=1)
 
 # Adding a button
-
 action = ttk.Button(window, text="Click Here", command=click_me)
-action.grid(column=0, row=1)
+action.grid(column=1, row=1)
 
 
 #======================

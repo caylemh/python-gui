f4d5343fd9f2b61167b9e0748a9be55c10bd09fe
caylemh <caylemh@gmail.com>
2020-08-25 11:02:43 +0200

    creating menubars & items

--- layouts/layout_1.py
+++ layouts/layout_1.py
@@ -4,6 +4,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import scrolledtext
+from tkinter import Menu
 
 # Create instance
 win = tk.Tk()   
@@ -11,9 +12,13 @@ win = tk.Tk()
 # Add a title       
 win.title("Python GUI")  
 
+# We are creating a container frame to hold all other widgets
+mighty = ttk.LabelFrame(win, text=' Mighty Python ')
+mighty.grid(column=0, row=0, padx=8, pady=4)
+
 # Modify adding a Label
-a_label = ttk.Label(win, text="A Label")
-a_label.grid(column=0, row=0)
+a_label = ttk.Label(mighty, text="A Label")
+a_label.grid(column=0, row=0, sticky='W')
 
 # Modified Button Click Function
 def click_me(): 
@@ -21,37 +26,37 @@ def click_me():
                      number_chosen.get())
 
 # Changing our Label
-ttk.Label(win, text="Enter a name:").grid(column=0, row=0)
+ttk.Label(mighty, text="Enter a name:").grid(column=0, row=0)
 
 # Adding a Textbox Entry widget
 name = tk.StringVar()
-name_entered = ttk.Entry(win, width=12, textvariable=name)
-name_entered.grid(column=0, row=1)
+name_entered = ttk.Entry(mighty, width=12, textvariable=name)
+name_entered.grid(column=0, row=1, sticky=tk.W)
 
 # Adding a Button
-action = ttk.Button(win, text="Click Me!", command=click_me)   
+action = ttk.Button(mighty, text="Click Me!", command=click_me)   
 action.grid(column=2, row=1)                                 # <= change column to 2
 
 # Creating three checkbuttons
-ttk.Label(win, text="Choose a number:").grid(column=1, row=0)
+ttk.Label(mighty, text="Choose a number:").grid(column=1, row=0)
 number = tk.StringVar()
-number_chosen = ttk.Combobox(win, width=12, textvariable=number, state='readonly')
+number_chosen = ttk.Combobox(mighty, width=12, textvariable=number, state='readonly')
 number_chosen['values'] = (1, 2, 4, 42, 100)
 number_chosen.grid(column=1, row=1)
 number_chosen.current(0)
 
 chVarDis = tk.IntVar()
-check1 = tk.Checkbutton(win, text="Disabled", variable=chVarDis, state='disabled')
+check1 = tk.Checkbutton(mighty, text="Disabled", variable=chVarDis, state='disabled')
 check1.select()
 check1.grid(column=0, row=4, sticky=tk.W)                   
 
 chVarUn = tk.IntVar()
-check2 = tk.Checkbutton(win, text="UnChecked", variable=chVarUn)
+check2 = tk.Checkbutton(mighty, text="UnChecked", variable=chVarUn)
 check2.deselect()
 check2.grid(column=1, row=4, sticky=tk.W)                   
 
 chVarEn = tk.IntVar()
-check3 = tk.Checkbutton(win, text="Enabled", variable=chVarEn)
+check3 = tk.Checkbutton(mighty, text="Enabled", variable=chVarEn)
 check3.deselect()
 check3.grid(column=2, row=4, sticky=tk.W)                     
 
@@ -71,7 +76,7 @@ chVarEn.trace('w', lambda unused0, unused1, unused2 : checkCallback())
 # Using a scrolled Text control    
 scrol_w  = 30
 scrol_h  =  3
-scr = scrolledtext.ScrolledText(win, width=scrol_w, height=scrol_h, wrap=tk.WORD)
+scr = scrolledtext.ScrolledText(mighty, width=scrol_w, height=scrol_h, wrap=tk.WORD)
 #### scr.grid(column=0, row=5, sticky='WE', columnspan=3)                    
 scr.grid(column=0, row=5, columnspan=3)              # sticky property removed   
 
@@ -96,12 +101,12 @@ radVar.set(99)
  
 # Now we are creating all three Radiobutton widgets within one loop
 for col in range(3):                             
-    curRad = tk.Radiobutton(win, text=colors[col], variable=radVar, value=col, command=radCall)          
+    curRad = tk.Radiobutton(mighty, text=colors[col], variable=radVar, value=col, command=radCall)          
     curRad.grid(column=col, row=6, sticky=tk.W)             # row=6
 
 # Create a container to hold labels
 buttons_frame = ttk.LabelFrame(win, text='Labels in a Frame')
-buttons_frame.grid(column=1, row=7, padx=20, pady=40) 
+buttons_frame.grid(column=0, row=7, padx=20, pady=10) 
 # buttons_frame.grid(column=1, row=7)                # now in col 1
  
 # Place labels into the container element
@@ -117,6 +122,27 @@ ttk.Label(buttons_frame, text="Label3").grid(column=0, row=2, sticky=tk.W)
 # Adding padding around label widgets
 for child in buttons_frame.winfo_children():
     child.grid_configure(padx=8, pady=4)
+
+# create quit function for the application
+def _quit():
+    win.quit()
+    win.destroy()
+    win.exit()
+
+# Adding a Menubar
+menu_bar = Menu(win)
+win.config(menu=menu_bar)
+
+# Create a menu and add menu items
+file_menu = Menu(menu_bar, tearoff=0) # create File menu
+file_menu.add_command(label="New") # create File menu item
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=_quit)
+menu_bar.add_cascade(label="File", menu=file_menu) # add File menu to menu bar & give it a label
+
+help_menu = Menu(menu_bar, tearoff=0) # create a Help menu
+menu_bar.add_cascade(label="Help", menu=help_menu)
+help_menu.add_command(label="About") # create a Help meu item
 
 name_entered.focus()      # Place cursor into name Entry
 #======================
